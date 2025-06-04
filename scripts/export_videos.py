a0c0bb5a9a3f43d4b06ea1a766c4fc9bb7b8595f
koddenbrock <koddenbrock@gmail.com>
2025-06-04 09:38:27 +0200

    Refactor video conversion process to support multiple data types and change frame extraction to grayscale

--- scripts/export_videos.py
+++ scripts/export_videos.py
@@ -5,20 +5,12 @@ import cv2
 
 from file_io.utils import extract_frames
 
-if __name__ == "__main__":
 
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    data_path = os.path.join(script_dir, '..', 'data', 'mpi')
-    output_path = os.path.join(script_dir, '..', 'data', 'mpi_converted')
-
-    os.makedirs(output_path, exist_ok=True)
-
-
+def convert_videos(data_path, output_path):
     video_files = glob(os.path.join(data_path, "*.avi")) + glob(os.path.join(data_path, "*.tif"))
-
     for video_path in video_files:
         print(f"Processing: {video_path}")
-        frames, fps = extract_frames(video_path)
+        frames, fps = extract_frames(video_path, color_mode="grayscale")
 
         # write frames to mp4 video
         base_name = os.path.splitext(os.path.basename(video_path))[0]
@@ -35,4 +27,16 @@ if __name__ == "__main__":
             writer.write(frame)
         writer.release()
         print(f"Video saved to: {video_output}")
+
+
+if __name__ == "__main__":
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    data_path_A = os.path.join(script_dir, '..', 'data', 'mpi', 'type_A')
+    data_path_B = os.path.join(script_dir, '..', 'data', 'mpi', 'type_B')
+    output_path = os.path.join(script_dir, '..', 'data', 'mpi_converted')
+    os.makedirs(output_path, exist_ok=True)
+
+    # convert_videos(data_path_A, output_path)
+    convert_videos(data_path_B, output_path)
 
