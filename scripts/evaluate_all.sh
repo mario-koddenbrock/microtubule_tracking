d9f024187225f614783fbe18eafdf06ffbc8935a
koddenbrock <koddenbrock@gmail.com>
2025-07-23 11:11:19 +0200

    feat(evaluation): comment out unused tuning configurations in evaluation script

--- scripts/evaluate_all.sh
+++ scripts/evaluate_all.sh
@@ -2,7 +2,7 @@
 
 python scripts/optimize_synthetic_data.py config/tuning_config_A_cosine.json --evaluate
 python scripts/optimize_synthetic_data.py config/tuning_config_A_fid.json --evaluate
-python scripts/optimize_synthetic_data.py config/tuning_config_A_jsd.json --evaluate
+# python scripts/optimize_synthetic_data.py config/tuning_config_A_jsd.json --evaluate
 python scripts/optimize_synthetic_data.py config/tuning_config_A_kid.json --evaluate
-python scripts/optimize_synthetic_data.py config/tuning_config_A_mahalanobis.json --evaluate
-python scripts/optimize_synthetic_data.py config/tuning_config_A_ndb.json --evaluate
+# python scripts/optimize_synthetic_data.py config/tuning_config_A_mahalanobis.json --evaluate
+# python scripts/optimize_synthetic_data.py config/tuning_config_A_ndb.json --evaluate
